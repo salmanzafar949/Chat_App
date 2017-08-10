72731469eef1c1d2ebadf61e5abbcf71c007d0fd
salman zafar <salmanzafar949@gmail.com>
2017-08-10 16:01:14 +0500

    Installed vue bootstap 4 and designed the Chat Page and Created a route for chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,32 @@
+ {{--  Adding link to style stylesheet  --}}
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+<div class="container">
+    <div class="row" id="app">
+         
+             <ul class="list-group offset-4 col-4">
+                <li class="list-group-item active">Chat Room</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+
+                <input type="text" class="form-control" placeholder="type your Message here...">
+              </ul>
+         
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<script src="{{ asset('js/app.js') }}"></script>
